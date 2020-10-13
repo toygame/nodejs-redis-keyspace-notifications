4a3c3486b5d68790f037e03982e162deb9c2127d
Thanapon <thanapon.toy@hotmail.com>
2020-10-13 22:28:33 +0700

    Add redis configuration

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
+// Redis configuration
+const ioRedis = require('ioredis')
+const host = 'localhost'
+const port = 6379
+const db = 0
+const password = ''
+/* Setup redis client and set key-space-notification event when key expired */
+const redis = new ioRedis({host, port, db, password})
+redis.on('ready', ()=> {
+  console.log('Redis connected!');
+  redis.config('SET', 'notify-keyspace-events', 'Ex')
+})
+
+const subscriber = new ioRedis({ host, port, db, password })
+
+subscriber.subscribe("__keyevent@0__:expired")
+subscriber.on('message',async (channel, message) => {
+  // do somethings
+  console.log(message);
+})
+//-------------------------//
+
 var app = express();
 
 // view engine setup
@@ -14,19 +36,21 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
